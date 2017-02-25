913239866e9cad3c02b1cc91e22d87a5dde76434
LuisJavierRT <lujaramireztorres@gmail.com>
2017-02-25 17:09:17 -0600

    BD 1.1

---
+++ bd/StoreProcedures.sql
@@ -0,0 +1,341 @@
+use `mydb`
+
+#SP para validar un usuario en el sistema
+delimiter $$
+create procedure sp_login
+(
+    pUsuario varchar(30),
+    pPassword varchar(30)
+)
+begin
+    declare passwordMD5 varchar(32);
+    
+    if exists(select u.usuario from Usuario u where u.activo = 1) then
+        set passwordMD5 = md5(pPassword);
+        
+        select u.usuario from Usuario u  
+        where (u.usuario = pUsuario) and (u.contraseña = passwordMD5);
+    end if;
+ 
+end $
+
+delimiter $$
+create procedure sp_agregarUsuario (
+
+	in _usuario varchar(30),
+    in _contraseña varchar(30), 
+	in _cedula varchar(11),
+	in _nombre varchar(60),
+	in _correo varchar(60),
+	in _tipo varchar(15),
+    in _activo bit,
+	in _fechaInicioAutorizacion datetime ,
+	in _fechaFinalAutorizacion datetime
+)
+begin
+	insert into Usuario (usuario, contraseña, cedula, nombre, correo, tipo, activo, fechaInicioAutorizacion, fechaFinalAutorizacion)
+    values (_usuario, _contraseña, _cedula, _nombre, _correo, _tipo, _activo, _fechaInicioAutorizacion, _fechaFinalAutorizacion);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarFuncionario (
+
+	in _id int,
+	in _cedula varchar(11), 
+	in _nombre varchar(60),
+	in _apellido1 VARCHAR(60),
+	in _apellido2 VARCHAR(60),
+	in _activo bit,
+	in _fechaNacimiento datetime,
+	in _areaEspecialidad varchar(60)
+)
+begin
+	insert into Funcionario (id, cedula, nombre, apellido1, apellido2, activo, fechaNacimiento, areaEspecialidad)
+    values (_id, _cedula, _nombre, _apellido1, _apellido2, _activo, _fechaNacimiento, _areaEspecialidad);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarDependencia (
+
+	 in _id int,
+	 in _codigo varchar(10), 
+	 in _nombre varchar(60)
+)
+begin
+	insert into Dependencia (id, codigo, nombre)
+    values (_id, _codigo, _nombre);
+end $$
+delimiter ;
+
+
+
+delimiter $$
+create procedure sp_agregarPerteneceFuncionarioDependencia (
+
+	 in _idFuncionario int,
+	 in _idDependencia int, 
+	 in _activo bit
+)
+begin
+	insert into Pertenece (idFuncionario, idDependencia, activo)
+    values (_idFuncionario, _idDependencia, _activo);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarAntecedente (
+	
+     in _id int,
+	 in _idFuncionario int,
+	 in _descripcion varchar(60)
+)
+begin
+	insert into Antecedente (id, idFuncionario, descripcion)
+    values (_id, _idFuncionario, _descripcion);
+end $$
+delimiter ;
+
+delimiter $$
+create procedure sp_agregarTitulo (
+	
+     in _id int,
+	 in _idFuncionario int,
+     in _titulo varchar(30),
+     in _universidad varchar(30),
+     in _gradoAcademico varchar(30),
+	 in _añoObtencion varchar(4)
+)
+begin
+	insert into Titulo (id, idFuncionario, titulo, universidad, gradoAcademico, añoObtencion)
+    values (_id, _idFuncionario, _titulo, _universidad, _gradoAcademico, _añoObtencion);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarPlaza (
+	
+     in _id int,
+     in _descripcion varchar(60),
+     in _fechaRegistro datetime,
+     in _activo bit
+)
+begin
+	insert into Plaza (id, descripcion, fechaRegistro, activo)
+    values (_id, _descripcion, _fechaRegistro, _activo);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarCaracteristicaPlaza (
+	
+     in _id int,
+	 in _idPlaza int, 
+	 in _codigo varchar(8),
+	 in _periodo varchar(30),  -- ??
+	 in _programa int,
+	 in _categoria int,   	   -- ??
+	 in _tce int,
+	 in _activo bit,
+	 in _puesto varchar(30),
+	 in _porcentajeCreacion int,
+	 in _asignacionDependencia bit,
+	 in _fechaAutorizacionInicio datetime,
+	 in _fechaAutorizacionFinal datetime,
+	 in _articulo varchar(10),
+	 in _numeroAcuerdo int,
+	 in _fechaAcuerdo datetime
+)
+begin
+	insert into CaracteristicaPlaza (id, idPlaza, codigo, periodo, programa, categoria, tce, activo, puesto, 
+    porcentajeCreacion, asignacionDependencia,fechaAutorizacionInicio, fechaAutorizacionFinal, articulo, numeroAcuerdo, fechaAcuerdo)
+    values (_id, _idPlaza, _codigo, _periodo, _programa, _categoria, _tce, _activo, _puesto, 
+    _porcentajeCreacion, _asignacionDependencia, _fechaAutorizacionInicio, _fechaAutorizacionFinal, _articulo, _numeroAcuerdo, _fechaAcuerdo);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarPlazaDependencia (
+	
+     in _id int,
+	 in _idPlaza int, 
+     in _idDependencia int,
+     in _porcentajeAcordado int,
+	 in _activo bit,
+	 in _fechaInicio datetime,
+	 in _fechaFinal datetime,
+	 in _indefinida bit,
+     in _descripcion varchar(60)
+)
+begin
+	insert into PlazaDependencia (id, idPlaza, idDependencia, porcentajeAcordado, activo, fechaInicio, fechaFinal, indefinida, descripcion)
+    values (_id, _idPlaza, _idDependencia, _porcentajeAcordado, _activo, _fechaInicio, _fechaFinal, _indefinida, _descripcion);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarResolucionRectoriaPlaza (
+
+	in _id int,
+    in _numeroResolucion int,
+    in _fechaResolucion datetime,
+    in _nombreRector varchar(60)
+)
+begin
+	insert into ResolucionRectoriaPlaza (id, numeroResolucion, fechaResolucion, nombreRector)
+    values (_id, _numeroResolucion, _fechaResolucion, _nombreRector);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarDatosResolucionPlaza (
+
+	in _idPlazaAfectada int,
+    in _idResolucionRectoriaPlaza int,
+    in _fechaInicioAfectacion datetime,
+    in _fechaFinalAfectacion datetime,
+    in _puestoAutorizado varchar(20),
+    in _categoríaAutorizada varchar(20),
+    in _porcentajeJornadaAutorizado int,
+    in _activo bit
+)
+begin
+	insert into DatosResolucionPlaza (idPlazaAfectada, idResolucionRectoriaPlaza, fechaInicioAfectacion, fechaFinalAfectacion, puestoAutorizado, categoríaAutorizada, porcentajeJornadaAutorizado, activo)
+    values (_idPlazaAfectada, _idResolucionRectoriaPlaza, _fechaInicioAfectacion, _fechaFinalAfectacion, _puestoAutorizado, _categoríaAutorizada, _porcentajeJornadaAutorizado, _activo);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarPlazaResolucionRectoriaPlaza (
+	in _id int,
+    in _idContratoLiberado int,
+    in _idDependencia int,
+    in _idFuncionario int,
+    in _activo bit,
+    in _actividad varchar(30),
+    in _descripcion varchar(40),
+    in _anno int,
+    in _fechaInicio datetime,
+    in _fechaFinal datetime,
+    in _númeroConcurso varchar(10),
+    in _suplencia bit,
+    in _porqueContratacion varchar(40),
+    in _quienNombro varchar(50),
+    in _puestoQuienNombro varchar(15),
+    in _porcentajeTotalContratacion int,
+    in _porcentajeLiberado int
+)
+begin
+	insert into PlazaResolucionRectoriaPlaza (idPlaza, idResolucionRectatoriaPlaza)
+    values (_idPlaza, _idResolucionRectoriaPlaza);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarResolucionRectoriaPersona (
+	in _id int,
+    in _idContrato int,
+    in _numeroResolucion int,
+    in _fechaResolucion datetime,
+    in _nombreRector varchar(50)
+)
+begin
+	insert into ResolucionRectoriaPersona (id, idContrato, numeroResolucion, fechaResolucion, nombreRector)
+    values (_id, _idContrato, _numeroResolucion, _fechaResolucion, _nombreRector);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarDatosResolucionPersona (
+	in _idPlazaAsignada int,
+    in _idResolucionPersona int,
+    in _fechaInicioAfectacion datetime,
+    in _fechaFinalAfectacion datetime,
+    in _puestoAutorizado varchar(20),
+    in _categoriaAutorizada varchar(20),
+    in _activo bit, 
+    in _porcentajeJornadaAutorizado int,
+	in _cedula int,
+    in _nombre varchar(15),
+    in _apellido1 varchar(15),
+    in _apellido2 varchar(15),
+    in _gradoAcademico varchar(40), 
+    in _codigoCursoAutorizado varchar(10)
+)
+begin
+	insert into DatosResolucionPersona (idPlazaAsignada, idResolucionPersona, fechaInicioAfectacion, fechaFinalAfectacion, puestoAutorizado, categoriaAutorizada, activo, porcentajeJornadaAutorizado, cedula, nombre, apellido1, apellido2, gradoAcademico, codigoCursoAutorizado)
+    values (_idPlazaAsignada, _idResolucionPersona, _fechaInicioAfectacion, _fechaFinalAfectacion, _puestoAutorizado, _categoriaAutorizada, _activo, _porcentajeJornadaAutorizado, _cedula, _nombre, _apellido1, _apellido2, _gradoAcademico, _codigoCursoAutorizado);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarAmpliacion (
+	in _id int,
+    in _idContrato int,
+    in _tipo varchar(15),
+    in _puestoQuienDioPermiso varchar(20),
+    in _quienDioPermiso varchar(50),
+    in _tienePermiso bit,
+    in _fechaInicio datetime,
+    in _fechaFinal datetime
+)
+begin
+	insert into Ampliacion (id, idContrato, tipo, puestoQuienDioPermiso, quienDioPermiso, tienePermiso, fechaInicio, fechaFinal)
+    values (_id, _idContrato, _tipo, _puestoQuienDioPermiso, _quienDioPermiso, _tienePermiso, _fechaInicio, _fechaFinal);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarContrato (
+	in _id int,
+    in _idContratoLiberado int,
+    in _idDependencia int,
+    in _idFuncionario int,
+    in _activo bit,
+    in _actividad varchar(30),
+    in _descripcion varchar(40),
+    in _anno int,
+    in _fechaInicio datetime,
+    in _fechaFinal datetime,
+    in _númeroConcurso varchar(10),
+    in _suplencia bit,
+    in _porqueContratacion varchar(40),
+    in _quienNombro varchar(50),
+    in _puestoQuienNombro varchar(15),
+    in _porcentajeTotalContratacion int,
+    in _porcentajeLiberado int
+)
+begin
+	insert into Contrato (id, idContratoLiberado, idDependencia, idFuncionario, activo, actividad, descripcion, anno, fechaInicio, fechaFinal, numeroConcurso, suplencia, porqueContratacion, quienNombro, puestoQuienNombro, porcentajeTotalContratacion, porcentajeLiberado)
+    values (_id, _idContratoLiberado, _idDependencia, _idFuncionario, _activo, _actividad, _descripcion, _anno, _fechaInicio, _fechaFinal, _numeroConcurso, _suplencia, _porqueContratacion, _quienNombro, _puestoQuienNombro, _porcentajeTotalContratacion, _porcentajeLiberado);
+end $$
+delimiter ;
+
+
+delimiter $$
+create procedure sp_agregarPlazaContratacion (
+	in _idPlaza int,
+    in _idContrato int,
+    in _idDependencia int,
+    in _idFuncionario int,
+    in _porcentajeContratacion int
+)
+begin 
+	insert into PlazaContratacion (idPlaza, idContrato, idDependencia, idFuncionario, porcentajeContratacion)
+    values (_idPlaza, _idContrato, _idDependencia, _idFuncionario, _porcentajeContratacion);
+end $$
+delimiter ;
---
+++ bd/scriptBD_SAA.sql
@@ -0,0 +1,304 @@
+CREATE SCHEMA `mydb` DEFAULT CHARACTER SET utf8;
+USE `mydb`;
+
+create table `mydb`.`Usuario` (
+  `usuario` VARCHAR(30) NOT NULL,
+  `contraseña` VARCHAR(30) NOT NULL, 
+  `cedula` VARCHAR(11) NOT NULL,
+  `nombre` VARCHAR(60) NOT NULL,
+  `correo` VARCHAR(60) NOT NULL,
+  `tipo` VARCHAR(15) NOT NULL,
+  `activo` bit NOT NULL,
+  `fechaInicioAutorizacion` DATETIME NOT NULL,
+  `fechaFinalAutorizacion` DATETIME NOT NULL,
+  PRIMARY KEY (`usuario`))
+ENGINE = InnoDB;
+
+create table `mydb`.`Funcionario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cedula` VARCHAR(11) NOT NULL, 
+  `nombre` VARCHAR(60) NOT NULL,
+  `apellido1` VARCHAR(60) NOT NULL,
+  `apellido2` VARCHAR(60) NOT NULL,
+  `activo` BIT NOT NULL DEFAULT 1,
+  `fechaNacimiento` DATETIME NOT NULL,
+  `areaEspecialidad` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+create table `mydb`.`Dependencia` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `codigo` VARCHAR(10) NOT NULL, 
+  `nombre` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+create table `mydb`.`Pertenece` (
+  `idFuncionario` INT NOT NULL AUTO_INCREMENT,
+  `idDependencia` INT NOT NULL AUTO_INCREMENT, 
+  `activo` BIT NOT NULL DEFAULT 1,
+  
+  PRIMARY KEY (`idFuncionario`,`idDependencia`),
+  
+  CONSTRAINT `fk_pertenece_funcionario`
+	FOREIGN KEY (`idFuncionario`)
+    REFERENCES `Funcionario` (`id`),
+  
+  CONSTRAINT `fk_pertenece_dependencia`
+    FOREIGN KEY (`idDependencia`)
+    REFERENCES `Dependencia` (`id`))
+ENGINE = InnoDB;
+
+create table `mydb`.`Antecedente` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `idFuncionario` INT NOT NULL AUTO_INCREMENT, 
+  `descripcion` VARCHAR(60) NOT NULL,
+  
+  PRIMARY KEY (`id`,`idFuncionario`),
+  
+  CONSTRAINT `fk_antecedente_funcionario`
+	FOREIGN KEY (`idFuncionario`)
+    REFERENCES `Funcionario` (`id`))
+ENGINE = InnoDB;
+
+create table `mydb`.`Titulo` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `idFuncionario` INT NOT NULL AUTO_INCREMENT, 
+  `titulo` VARCHAR(30) NOT NULL,
+  `universidad` VARCHAR(30) NOT NULL,
+  `gradoAcademico` VARCHAR(30) NOT NULL,
+  `añoObtencion` VARCHAR(4) NOT NULL,
+  
+  PRIMARY KEY (`id`,`idFuncionario`),
+  
+  CONSTRAINT `fk_titulo_funcionario`
+	FOREIGN KEY (`idFuncionario`)
+    REFERENCES `Funcionario` (`id`))
+ENGINE = InnoDB;
+
+
+create table `mydb`.`Plaza` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `descripcion` VARCHAR(60) NOT NULL, 
+  `fechaRegistro` DATETIME NOT NULL,
+  `activo` BIT NOT NULL DEFAULT 1,
+  
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+create table `mydb`.`CaracteristicaPlaza` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `idPlaza` INT NOT NULL AUTO_INCREMENT, 
+  `codigo` VARCHAR(8) NOT NULL,
+  `periodo` VARCHAR(30) NOT NULL,  -- ??
+  `programa` INT NOT NULL,
+  `categoria` INT NOT NULL,   	   -- ??
+  `tce` INT NOT NULL,
+  `activo` BIT NOT NULL DEFAULT 1,
+  `puesto` VARCHAR(30) NOT NULL,
+  `porcentajeCreacion` INT NOT NULL,
+  `asignacionDependencia` BIT NOT NULL DEFAULT 0,
+  `fechaAutorizacionInicio` DATETIME NOT NULL,
+  `fechaAutorizacionFinal` DATETIME NOT NULL,
+  `articulo` VARCHAR(10) NOT NULL,
+  `numeroAcuerdo` INT NOT NULL,
+  `fechaAcuerdo` DATETIME NOT NULL,
+  
+  PRIMARY KEY (`id`,`idPlaza`),
+  
+  CONSTRAINT `fk_caracteristica_plaza`
+	FOREIGN KEY (`idPlaza`)
+    REFERENCES `Plaza` (`id`))
+ENGINE = InnoDB;
+
+create table `mydb`.`PlazaDependencia` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `idPlaza` INT NOT NULL AUTO_INCREMENT, 
+  `idDependencia` INT NOT NULL AUTO_INCREMENT,
+  `porcentajeAcordado` INT NOT NULL,
+  `activo` BIT NOT NULL DEFAULT 1,
+  `fechaInicio` DATETIME NOT NULL,
+  `fechaFinal` DATETIME NOT NULL,
+  `indefinida` BIT NOT NULL DEFAULT 0,
+  `descripcion` VARCHAR(60) NOT NULL,
+  
+  PRIMARY KEY (`id`,`idPlaza`,`idDependencia`),
+  
+  CONSTRAINT `fk_plazaDependencia_plaza`
+	FOREIGN KEY (`idPlaza`)
+    REFERENCES `Plaza` (`id`),
+  
+  CONSTRAINT `fk_plazaDependencia_dependencia`
+    FOREIGN KEY (`idDependencia`)
+    REFERENCES `Dependencia` (`id`))
+ENGINE = InnoDB;
+
+create table `mydb`.`ResolucionRectoriaPlaza` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `numeroResolucion` INT NOT NULL, 
+  `fechaResolucion` DATETIME NOT NULL,
+  `nombreRector` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+create table `mydb`.`DatosResolucionPlaza` (
+	`idPlazaAfectada` int not null,
+    `idResolucionRectoriaPlaza` int not null,
+    `fechaInicioAfectacion` datetime not null,
+    `fechaFinalAfectacion` datetime not null,
+    `puestoAutorizado` varchar(20) not null,
+    `categoríaAutorizada` varchar(20) not null,
+    `porcentajeJornadaAutorizado` int not null,
+    `activo` bit not null,
+    primary key (`idPlazaAfectada`,	`idResolucionRectoriaPlaza`),
+    
+    constraint `fk_idPlazaAfectada` 
+		foreign key (`idPlazaAfectada`) 
+        references `mydb`.`Plaza` (`id`),
+        
+	constraint `fk_idResolucionRectoriaPlaza`
+		foreign key (`idResolucionRectoriaPlaza`) 
+        references `mydb`.`ResolucionRectoriaPlaza` (`id`)
+)
+engine = innodb;
+
+
+create table `mydb`.`PlazaResolucionRectoriaPlaza` (
+	`idPlaza` int not null,
+    `idResolucionRectoriaPlaza` int not null,
+    primary key (`idPlaza`, `idResolucionRectoriaPlaza`),
+    
+    constraint `fk_idPlaza`
+		foreign key (`idPlaza`)
+        references `mydb`.`Plaza` (`id`),
+        
+	constraint `fk_idResolucionRectoriaPlaza`
+		foreign key (`idResolucionRectoriaPlaza`)
+        references `mydb`.`ResolucionRectoriaPlaza` (`id`)
+)
+engine = innodb;
+
+
+create table `mydb`.`ResolucionRectoriaPersona` (
+	`id` int not null auto_increment,
+    `idContrato` int not null,
+    `idDependecia` int not null,
+    `idFuncionario` int not null,
+    `numeroResolucion` int not null,
+    `fechaResolucion` datetime not null,
+    `nombreRector` varchar(50) not null,
+    
+    primary key (`id`),
+    
+    constraint `fk_idContratoDependenciaFuncionario`
+		foreign key (`idContrato`, `idDependecia`, `idFuncionario`)
+        references `mydb`.`Contrato` (`id`, `idDependencia`, `idFuncionario`)
+)
+engine = innodb;
+
+
+create table `mydb`.`DatosResolucionPersona` (
+	`idPlazaAsignada` int not null,
+    `idResolucionPersona` int not null,
+    `fechaInicioAfectacion` datetime not null,
+    `fechaFinalAfectacion` datetime not null,
+    `puestoAutorizado` varchar(20) not null,
+    `categoriaAutorizada` varchar(20) not null,
+    `activo` bit not null, 
+    `porcentajeJornadaAutorizado` int not null,
+    `cedula` int not null,
+    `nombre` varchar(15) not null,
+    `apellido1` varchar(15) not null,
+    `apellido2` varchar(15) not null,
+    `gradoAcademico` varchar(40) not null, 
+    `codigoCursoAutorizado` varchar(10) not null,
+    
+    primary key (`idPlazaAsignada`, `idResolucionPersona`),
+    
+    constraint `fk_idPlazaAsignada`
+		foreign key (`idPlazaAsignada`)
+        references `mydb`.`Plaza` (`id`),
+        
+	constraint `fk_idResolucionPersona`
+		foreign key (`idResolucionPersona`)
+        references `mydb`.`ResolucionRectoriaPersona` (`id`)
+)
+engine = innodb;
+
+
+create table `mydb`.`Ampliacion` (
+	`id` int not null auto_increment,
+    `idContrato` int not null,
+    `idDependencia` int not null,
+    `idFuncionario` int not null,
+    `tipo` varchar(15) not null,
+    `puestoQuienDioPermiso` varchar(20) not null,
+    `quienDioPermiso` varchar(50) not null,
+    `tienePermiso` bit not null,
+    `fechaInicio` datetime not null,
+    `fechaFinal` datetime not null,
+    
+    primary key (`id`),
+    
+    constraint `fk_idContratoDependenciaFuncionario`
+		foreign key (`idContrato`, `idDependencia`, `idFuncionario`)
+		references `mydb`.`Contrato` (`id`, `idDependencia`, `idFuncionario`)
+)
+engine = innodb;
+
+
+create table `mydb`.`Contrato` (
+	`id` int not null auto_increment,
+    `idContratoLiberado` int null,
+    `idDependencia` int not null,
+    `idFuncionario` int not null,
+    `activo` bit not null,
+    `actividad` varchar(30) not null,
+    `descripcion` varchar(40) not null,
+    `anno` int not null,
+    `fechaInicio` datetime not null,
+    `fechaFinal` datetime not null,
+    `númeroConcurso` varchar(10) not null,
+    `suplencia` bit not null,
+    `porqueContratacion` varchar(40) not null,
+    `quienNombro` varchar(50) not null,
+    `puestoQuienNombro` varchar(15) not null,
+    `porcentajeTotalContratacion` int not null,
+    `porcentajeLiberado` int null,
+    
+    primary key (`id`, `idDependencia`, `idFuncionario`),
+    
+    constraint `fk_idDependencia`
+		foreign key (`idDependencia`)
+        references `mydb`.`Dependencia` (`id`),
+        
+	constraint `fk_idFuncionario`
+		foreign key (`idFuncionario`)
+        references `mydb`.`Funcionario`,
+        
+	constraint `fk_idContratoLiberado`
+		foreign key (`idContratoLiberado`)
+        references `mydb`.`Contrato` (`idContratoLiberado`)
+)
+engine = innodb;
+
+
+create table `mydb`.`PlazaContratacion` (
+	`idPlaza` int not null,
+    `idContrato` int not null,
+    `idDependencia` int not null,
+    `idFuncionario` int not null,
+    `porcentajeContratacion` int not null,
+    
+    primary key (`idCaracteristicaPlaza`, `idPlaza`, `idContrato`, `idDependencia`, `idFuncionario`),
+    
+    constraint `fk_idCaracteristicaPlaza`
+		foreign key (`idCaracteristicaPlaza`, `idPlaza`)
+        references `mydb`.`CaracteristicaPlaza` (`id`, `idPlaza`),
+	
+	constraint `fk_idContrato`
+		foreign key (`idContrato`, `idDependencia`, `idFuncionario`)
+        references `mydb`.`Contrato` (`id`, `idDependencia`, `idFuncionario`)
+)
+engine = innodb; 
